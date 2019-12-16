88859512e42fa9d6c444723149e36a2cb36289b3
bogdang989 <bogdan.gavrilovic@sbgenomics.com>
2019-12-16 17:52:55 +0100

    Remove old bindings

--- rabix-bindings-sb/src/main/java/org/rabix/bindings/sb/processor/SBPortProcessorCallback.java
+++
@@ -1,9 +0,0 @@
-package org.rabix.bindings.sb.processor;
-
-import org.rabix.bindings.model.ApplicationPort;
-
-public interface SBPortProcessorCallback {
-
-  SBPortProcessorResult process(Object value, String id, Object schema, Object binding, ApplicationPort parentPort) throws Exception;
-  
-}
